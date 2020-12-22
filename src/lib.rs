65a68f368c8c9156c95065fd9eb0d2179bbd810b
Tobias Hunger <tobias.hunger@gmail.com>
2020-12-22 15:53:08 +0100

    Move some type definitions out of download.rs
    ... and into progres.rs or verify.rs. They just fit better ofer there.

--- src/lib.rs
+++ src/lib.rs
@@ -30,8 +30,10 @@ pub mod downloader;
 pub mod progress;
 pub mod verify;
 
-pub use download::{Download, Progress, SimpleProgress, Verify};
+pub use download::Download;
 pub use downloader::Downloader;
+pub use progress::Progress;
+pub use verify::{SimpleProgress, Verify};
 
 // ----------------------------------------------------------------------
 // - Error:
