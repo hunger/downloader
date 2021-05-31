021f7e431f225b53e1548b99e9639eeaf281da55
phnixir <67435287+phnixir@users.noreply.github.com>
2021-05-31 22:20:48 +0430

    Minor touchups
    - Add a new copyright clause
    - Add a new demonstration of how the library can infer filenames of downloads

--- examples/tui_basic.rs
+++ examples/tui_basic.rs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2020 Tobias Hunger <tobias.hunger@gmail.com>
+// Copyright (C) 2021 Phoenix IR <ayitsmephoenix@firemail.cc>
 
 // Setup warnings/errors:
 #![forbid(unsafe_code)]
@@ -18,10 +19,16 @@ fn main() {
         .build()
         .unwrap();
 
+    // Download with an explicit filename
     let dl = downloader::Download::new("https://example.org/")
         .file_name(std::path::Path::new("example.html"));
+    
+    // Download with an inferred filename
+    let dl2 = downloader::Download::new(
+        "https://cdimage.debian.org/debian-cd/current/i386/iso-cd/debian-10.9.0-i386-netinst.iso",
+    );
 
-    let result = downloader.download(&[dl]).unwrap();
+    let result = downloader.download(&[dl,dl2]).unwrap();
 
     for r in result {
         match r {
